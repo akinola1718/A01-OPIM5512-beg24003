956445ddae6a3d69c1d1d6f44d53003e79027d30
akinola1718 <oluwaseun.akinola@uconn.edu>
2026-01-22 10:35:46 -0500

    Add CA housing boxplot script and output figure
    latest update

--- src/boxplot.py
+++ src/boxplot.py
@@ -1,11 +1,18 @@
 from sklearn.datasets import fetch_california_housing
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Load California Housing dataset
 housing = fetch_california_housing(as_frame=True)
 
 # Features + target as a single DataFrame
 df = housing.frame
+
+df.boxplot(column=["HouseAge", "MedInc"])
+plt.savefig('../figs/boxplot.png', dpi=2000)
+
+# Optional: Display the plot
+plt.show()
 
 # Quick check
 print(df.head())
